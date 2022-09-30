83a159d5590256d7e47320dce0435b1aa06e53a6
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-30 18:42:42 +0300

    adding apis convert to binary

--- app/Http/Controllers/ApisController.php
+++ app/Http/Controllers/ApisController.php
@@ -6,21 +6,39 @@ use Illuminate\Http\Request;
 
 class ApisController extends Controller
 {
-    function stringSort(Request $request){
+    function decomposeNumber(Request $request)
+    {
         $number = $request->number;
-        $arr=[];
-        $x=10;
-        while ($number>0){
-            $rest=$number%$x;
-            array_push($arr,$rest);
-            $number=$number-$rest;
-            $x=$x*10;
+        $arr = [];
+        $x = 10;
+        while ($number > 0) {
+            $rest = $number % $x;
+            array_push($arr, $rest);
+            $number = $number - $rest;
+            $x = $x * 10;
         }
 
-        return response()-> json([
-            "status"=>"success",
-            "number"=> $number,
-            "decompose number"=>$arr
+        return response()->json([
+            "status" => "success",
+            "number" => $number,
+            "decompose number" => $arr
         ]);
     }
-}
+
+    function toBinary(Request $request)
+    {
+        $input = $request->input;
+        $patterns = [
+            '/\b[0-9]+\b/' => function ($matches) {
+                return decbin($matches[0]);
+            }
+        ];
+        $result = preg_replace_callback_array($patterns, $input);
+        
+        return response()->json([
+            "status" => "success",
+            "to_binary" => $result
+        ]);
+    }
+
+    
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApisController;
 
-Route::post("/string_sort", [ApisController::class, 'stringSort']);
+Route::post("/decompose_number", [ApisController::class, 'decomposeNumber']);
+Route::post("/sort_string", [ApisController::class, 'sortString']);
+Route::post("/to_binary", [ApisController::class, 'toBinary']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
