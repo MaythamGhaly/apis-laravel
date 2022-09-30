92464ebdb7b0c97aee61dbd859c537e6e248a051
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-30 15:55:40 +0300

    adding api return place value

--- app/Http/Controllers/ApisController.php
+++ app/Http/Controllers/ApisController.php
@@ -4,9 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TestController extends Controller
+class ApisController extends Controller
 {
-    function getUsers(){
-        return "hi";
+    function stringSort(Request $request){
+        $number = $request->number;
+        $arr=[];
+        $x=10;
+        while ($number>0){
+            $rest=$number%$x;
+            array_push($arr,$rest);
+            $number=$number-$rest;
+            $x=$x*10;
+        }
+
+        return response()-> json([
+            "status"=>"success",
+            "number"=> $number,
+            "decompose number"=>$arr
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApisController;
 
-
-Route::post("/string_sort", [TestController::class, 'stringSort']);
+Route::post("/string_sort", [ApisController::class, 'stringSort']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
